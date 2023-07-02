f6660b2e59c0b534622ecfbe8abed744ed9745c6
simi <alchemy.officialz@gmail.com>
2023-07-02 13:59:47 +0530

    login validation

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../service/data.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -8,16 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-  formName:string = 'Login Here'
+  formName: string = 'Login Here'
   placeholderName = 'enter password'
-  constructor(private a1: Router) {
-
-  }
+  constructor(private a1: Router, private fb: FormBuilder) { }
+  loginForm = this.fb.group({
+    acno: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+    psw: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]]
+  })
   ngOnInit(): void {
 
   }
-  method1() {
-    this.a1.navigateByUrl('home')
+  login() {
+    if(this.loginForm.valid){
+      this.a1.navigateByUrl('home')
+    }
+    else{
+      alert('ínvalid form')
+    }
   }
 
 
